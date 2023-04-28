5bb2db40cfbdad024b5c08b4db8a15635f07bc49
masupa <dmasupa4@gmail.com>
2023-04-28 17:24:24 +0200

    Added deployment script

---
+++ Workflow-Orchestration/Prefect/etl-deployment.py
@@ -0,0 +1,13 @@
+from etl import etl # Import Flow function
+from prefect.deployments import Deployment
+
+from datetime import timedelta
+
+# Deployment configurations
+deployment = Deployment.build_from_flow(
+    flow=etl,
+    name="etl-deployment-2",
+)
+
+if __name__ == "__main__":
+    deployment.apply()
